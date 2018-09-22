b20d7c1fbff0439ea1d6d323446b5926f922e6dc
nadav <tashernadav@gmail.com>
2018-09-22 11:17:15 +0300

    V0.1

--- src/nadav/tasher/frc/simulator/Main.java
+++ src/nadav/tasher/frc/simulator/Main.java
@@ -1,5 +1,9 @@
 package nadav.tasher.frc.simulator;
 
+import nadav.tasher.frc.simulator.simulation.SimulationGUI;
+
+import javax.swing.*;
+
 public class Main {
     /*
     Written By Nadav Tasher Of General Angels
@@ -7,7 +11,27 @@ public class Main {
      */
     static final String programName = "2230 Simulator";
     static final double programVersion = 0.1;
+    private static JFrame mainFrame;
 
     public static void main(String[] args) {
+        loadTheme();
+        loadGUI();
+    }
+
+    private static void loadTheme() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+            Utils.tellUser("Failed To Set Desired Theme.");
+        }
+    }
+
+    private static void loadGUI() {
+        mainFrame = new JFrame(programName + " " + programVersion);
+        mainFrame.setUndecorated(true);
+        mainFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        mainFrame.setContentPane(new SimulationGUI());
+        mainFrame.setVisible(true);
     }
 }
---
+++ src/nadav/tasher/frc/simulator/simulation/SimulationGUI.java
@@ -0,0 +1,14 @@
+package nadav.tasher.frc.simulator.simulation;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SimulationGUI extends JPanel {
+    public SimulationGUI() {
+        init();
+    }
+
+    private void init() {
+        setBackground(Color.BLACK);
+    }
+}
